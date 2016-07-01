cf02cc9286572ff53ea0e6d622f01818a067afcc
xcatliu <xcatliu@gmail.com>
2016-07-01 18:31:57 +0800

    First version

---
+++ README.md
@@ -0,0 +1,37 @@
+# hexo-filter-date-from-git
+
+Read git log and overwrite the front-matter properties `date` and `updated` for each posts.
+
+**WARNING:** This plugin will **overwrite** the front-matter, **even if** you manually set a `date` or `updated` in the front-matter.
+
+A live site using this plugin: http://js-index.com/
+
+And the GitHub repo for that site: https://github.com/xcatliu/js-index
+
+## Installation
+
+```shell
+$ npm install hexo-filter-date-from-git --save
+```
+
+## Usage
+
+The `date` property will be the first time when you submitted the post. Same result as:
+
+```shell
+git log --format="%ad" -- <file> | tail -1
+```
+
+The `updated` property will be the last time when you submitted the post. Same result as:
+
+```shell
+git log --format="%ad" -- <file> | tail -1
+```
+
+**NOTICE:** If you created a new file, and before you submitted the file, the `date` and `updated` will be the time you run your hexo command.
+
+## Options
+
+Null
+
+[hexo-generator-archive]: https://github.com/hexojs/hexo-generator-archive
---
+++ index.js
@@ -0,0 +1,36 @@
+/* global hexo */
+/* eslint no-param-reassign:0, strict:0 */
+'use strict';
+
+const path = require('path');
+const execSync = require('child_process').execSync;
+const moment = require('moment-timezone');
+
+hexo.extend.filter.register('before_post_render', data => {
+  data.date = getDate(data);
+  data.updated = getUpdated(data);
+  return data;
+});
+
+function getDate(data) {
+  const filePath = getFilePath(data);
+  const date = execSync(`git log --format="%ad" -- ${filePath} | tail -1`).toString().trim();
+  // If the file is created a moment ago, it will be an untracked file, then git can not log it
+  if (date === '') {
+    return moment();
+  }
+  return moment(new Date(date));
+}
+
+function getUpdated(data) {
+  const filePath = getFilePath(data);
+  const updated = execSync(`git log -1 --format="%ad" -- ${filePath}`).toString().trim();
+  if (updated === '') {
+    return moment();
+  }
+  return moment(new Date(updated));
+}
+
+function getFilePath(data) {
+  return path.join(':', hexo.config.source_dir, data.source);
+}
