7ba01868e384752878d8972ab8f3cdb5cd5385d1
YQ <37969157+yqs112358@users.noreply.github.com>
2024-11-21 15:10:48 +0800

    Fix path space bug & Parse front-matter to determine whether to apply git date (#4)
    * fix: process failed when filePath contains space
    
    * chore: print process log to console
    
    * feat: parse front-matter to check whether date & updated exists
    
    * fix: improve the check of invalid date & updated

--- index.js
+++ index.js
@@ -2,29 +2,32 @@
 /* eslint no-param-reassign:0, strict:0 */
 'use strict';
 
+const log = hexo.log || log.log;
 const path = require('path');
 const execSync = require('child_process').execSync;
 const moment = require('moment-timezone');
+const front = require('hexo-front-matter');
 
 hexo.extend.filter.register('before_post_render', data => {
-  const originDate = data.date;
-  const gitDate = getDate(data);
-  if (gitDate < originDate) {
-    data.date = gitDate;
-  }
+  const filePath = getFilePath(data);
+  const [originalDate, originalUpdated] = getOriginalDateMeta(data)
 
-  const originUpdated = data.updated;
-  const gitUpdated = getUpdated(data);
-  if (gitUpdated < originUpdated) {
-    data.updated = gitUpdated;
+  if (!originalDate || originalDate.valueOf() <= 1000) {
+    // original "date" is invalid
+    data.date = getDate(filePath);
+    log.log(`Post ${filePath}  set "date" to ${data.date.format('YYYY-MM-DD HH:mm:ss')}`);
+  }
+  if (!originalUpdated || originalUpdated.valueOf() <= 1000) {
+    // original "updated" is invalid
+    data.updated = getUpdated(filePath);
+	  log.log(`Post ${filePath}  set "updated" to ${data.updated.format('YYYY-MM-DD HH:mm:ss')}`);
   }
 
   return data;
 });
 
-function getDate(data) {
-  const filePath = getFilePath(data);
-  const date = execSync(`git log --follow --format="%ad" -- ${filePath} | tail -1`).toString().trim();
+function getDate(filePath) {
+  const date = execSync(`git log --follow --format="%ad" -- "${filePath}" | tail -1`).toString().trim();
   // If the file is created a moment ago, it will be an untracked file, then git can not log it
   if (date === '') {
     return moment();
@@ -32,9 +35,8 @@ function getDate(data) {
   return moment(new Date(date));
 }
 
-function getUpdated(data) {
-  const filePath = getFilePath(data);
-  const updated = execSync(`git log --follow -1 --format="%ad" -- ${filePath}`).toString().trim();
+function getUpdated(filePath) {
+  const updated = execSync(`git log --follow -1 --format="%ad" -- "${filePath}"`).toString().trim();
   if (updated === '') {
     return moment();
   }
@@ -43,4 +45,11 @@ function getUpdated(data) {
 
 function getFilePath(data) {
   return path.resolve(hexo.config.source_dir, data.source);
+}
+
+function getOriginalDateMeta(data) {
+  const frontMatter = front.parse(data.raw);
+  if (frontMatter == null)
+    return [null, null]
+  return [frontMatter.date, frontMatter.updated]
 }
